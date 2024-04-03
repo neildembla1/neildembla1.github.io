16947462d246063ba564df64a9f5061df5056e50
NeilLaptop <techyboyey@gmail.com>
2024-04-03 00:17:37 -0400

    renamed files and made navbar not sticky to the top

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ fetch('/assets/nav.html')
 	.then(text => {
 		let oldElem = document.querySelector("script#replace_with_navbar");
 		let newElem = document.createElement("nav");
-		newElem.className = "navbar navbar-expand-lg sticky-top bg-primary";
+		newElem.className = "navbar navbar-expand-lg bg-primary"; //sticky-top
 		newElem.innerHTML = text;
 		oldElem.parentNode.replaceChild(newElem, oldElem);
 	});
