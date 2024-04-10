ba613b321d5baba25bd5e403138d2c4c352e630c
Neil Dembla <neildembla1@gmail.com>
2024-04-10 19:47:39 -0400

    fixing classList error in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 // This lets footers stick to the bottom
-document.getElementsByTagName("body")[0].classList += "d-flex flex-column min-vh-100";
+document.getElementsByTagName("body")[0].classList += " d-flex flex-column min-vh-100";
 // This adds a bit of space before the footer
-document.getElementsByTagName("main")[0].classList += "mb-5";
+document.getElementsByTagName("main")[0].classList += " mb-5";
